9ca981636b7aa19f6e631bbe90c766a2293b6cbf
lucas <lucas.s.s.silva@hotmail.com>
2020-09-14 15:57:05 -0300

    Tratando saída de dados

---
+++ ExercicioFixacao/ExercicioFixacao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos: \n{0}, curjo preco é $ {1:F2} \n{2}, cujo preço é {3:F2}\n", produto1, preco1, produto2, preco2);
+            Console.WriteLine("Registro: {0} anos de idade, código {1} e gênero: {2}\n", idade, codigo, genero);
+            Console.WriteLine("Medida com oito casas decimais: {0}",medida.ToString("F8"));
+            Console.WriteLine("Arredondado (três casas decimais): {0}", medida.ToString("F3"));
+            Console.WriteLine("Separador decimal invariant culture: {0}", medida.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char genero = 'F';
+            int idade = 20;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo.ToString("F2")); // limitar o número de casas depois da virgula
+            Console.WriteLine(nome);
+            Console.WriteLine("Saldo com 4 casas: " + saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture)); // mudar o separador de decimais
+            Console.WriteLine("");
+            Console.WriteLine("=============================================================");
+            Console.WriteLine("");
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a R${2:F2} reais",nome, idade, saldo); // PLACEHOLDER
+
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais"); // INTERPOLAÇÃO
+
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2") + " reais"); //Concatenacao
+        }
+    }
+}
