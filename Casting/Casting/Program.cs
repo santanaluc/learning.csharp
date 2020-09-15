ae67741756c0283ffbd590c10b9b1d97ce5ef5f4
lucas <lucas.s.s.silva@hotmail.com>
2020-09-15 17:04:25 -0300

    novos conhecimentos

---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x = 4.5f;
+
+            double y = x; // conversão implicita de tipos
+            //float = 4 bytes e double 8bytes
+
+            Console.WriteLine(y);
+
+            double a;
+            int b;
+
+            a = 5.6;
+            b = (int)a; //conversão explicita de tipo (casting)
+            // se mudar um valor float/double para int, o valor é truncado
+            Console.WriteLine(b);
+
+            int c = 5;
+            int d = 2;
+
+            double resultado = (double) c / d;
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
---
+++ OperadorAtribuicao/OperadorAtribuicao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OperadorAtribuicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            string s = "ABC";
+            Console.WriteLine(s);
+
+            s += "DEF";
+            Console.WriteLine(s);
+
+            a++;
+            Console.WriteLine(a);
+
+            a--;
+            Console.WriteLine(a);
+        }
+    }
+}
