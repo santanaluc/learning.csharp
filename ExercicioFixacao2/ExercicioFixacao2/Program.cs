d5048044453a08321b3a91d4ed8929649ff61ad3
lucas <lucas.s.s.silva@hotmail.com>
2020-09-18 17:10:08 -0300

    Exercicio proposto c/ base aulas anteriores

---
+++ ExercicioFixacao2/ExercicioFixacao2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExercicioFixacao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            float preco = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com seu ultimo nome, idade e altura:");
+            string[] vetor = Console.ReadLine().Split(' ');
+            string sobrenome = vetor[0];
+            int idade = int.Parse(vetor[1]);
+            double altura = double.Parse(vetor[2]);
+
+            Console.WriteLine("=================================");
+            Console.WriteLine(nome + "\n" + quartos + "\n" + preco.ToString("F2") + "\n" + sobrenome + "\n" + idade + "\n" + altura.ToString("F2"));
+        }
+    }
+}
