2e171f4cb66e5a5fbac6d73e2a7481bfa073b6da
lucas <lucas.s.s.silva@hotmail.com>
2020-09-18 17:04:14 -0300

    Utilizando Console.ReadLine()

---
+++ EntradaDados/EntradaDados/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EntradaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine(), y = Console.ReadLine(), z = Console.ReadLine();
+
+            //utilizando vetores
+            string[] vet = Console.ReadLine().Split(' ');
+            string a = vet[0];
+            string b = vet[1];
+            string c = vet[2];
+
+            Console.WriteLine("Voce digitou: " + frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            //imprimindo o vetor
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
