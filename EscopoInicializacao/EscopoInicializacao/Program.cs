398e5dd2b2cc8d4dce73bc86eb7d2ee4a0ec9a89
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-09-28 17:33:46 -0300

    Entendo mais sobre inicializacao de variavel

---
+++ EscopoInicializacao/EscopoInicializacao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EscopoInicializacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o preço: ");
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = 0.0;
+
+            if (preco > 100.0)
+            {
+                desconto = preco * 0.1;
+            }
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
