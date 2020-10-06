a64799239e550c2ed15d9d8655c88019076b0796
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-06 19:17:22 -0300

    Recaptulando estrutura condicional while

---
+++ While/While/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            double x = double.Parse(Console.ReadLine());
+
+            while (x >= 0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3"));
+                Console.Write("Digite outro número: ");
+                x = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Número negativo!");
+        }
+    }
+}
