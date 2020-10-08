e2734792b3f033e2b327f04f8654e5b11053cc4e
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-08 17:03:12 -0300

    Aprendendo sobre membros estáticos e adicionando ao código

---
+++ MembrosEstaticos/MembrosEstaticos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circuferência: " + circ.ToString("F2"));
+            Console.WriteLine("Volume: " + volume.ToString("F2"));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2"));
+        }
+    }
+}
