532cd49f3a2e454f9ac6217777e869fb76dcbdd2
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-08 03:15:36 -0300

    Utilizando classe, atributos e metodos no exercicio de fixação de POO com C#

---
+++ ExerciciosPOO/Exercicio03/Exercicio03/Aluno.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio03
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+        public double NotaFinal;
+
+        public double CalcularNota()
+        {
+            return NotaFinal = (Nota1 + Nota2 + Nota3);
+        }
+
+        public bool Aprovado()
+        {
+            if (NotaFinal >= 60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double SituacaoFinal()
+        {
+            if (Aprovado())
+            {
+                return 0;
+            } else
+            {
+                return 60 - NotaFinal;
+            }
+        }
+    }
+}
---
+++ ExerciciosPOO/Exercicio03/Exercicio03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            a.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            a.Nota1 = double.Parse(Console.ReadLine());
+            a.Nota2 = double.Parse(Console.ReadLine());
+            a.Nota3 = double.Parse(Console.ReadLine());
+
+            if (a.Nota1 > 30 || a.Nota2 > 35 || a.Nota3 > 35)
+            {
+                Console.WriteLine("Verifique as notas digitadas e tente novamente");
+                return;
+            } else
+            {
+                Console.WriteLine("NOTA FINAL = " + a.CalcularNota().ToString("F2"));
+
+                if (a.Aprovado())
+                {
+                    Console.WriteLine("APROVADO");
+                }
+                else
+                {
+                    Console.WriteLine("Reprovado!\nFaltaram " + a.SituacaoFinal().ToString("F2") + " pontos");
+                }
+            }            
+        }
+    }
+}
