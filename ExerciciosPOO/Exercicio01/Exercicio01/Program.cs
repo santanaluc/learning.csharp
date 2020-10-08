c1b20c29d0b58a2c48f866cde5f54d3697b50d09
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-08 01:45:50 -0300

    Exercícios propostos para a prática de programcao orientada a objetos

---
+++ ExerciciosPOO/Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura do retângulo:");
+            ret.Largura = double.Parse(Console.ReadLine());
+            ret.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("ÁREA = " + ret.Area().ToString("F2"));
+            Console.WriteLine("PERÍMETRO = " + ret.Perimetro().ToString("F2"));
+            Console.WriteLine("DIAGONAL = " + ret.Diagonal().ToString("F2"));
+        }
+    }
+}
---
+++ ExerciciosPOO/Exercicio01/Exercicio01/Retangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio01
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return (Largura * Altura);
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+}
