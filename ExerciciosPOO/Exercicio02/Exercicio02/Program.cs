477f085b08cc7ea1d63ab9926cd2af1a74d63da1
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-08 02:14:16 -0300

    Utilizando classe, atributos e metodos no exercicio de fixação de POO com C#

---
+++ ExerciciosPOO/Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionario: " + f.Nome + ", $" + f.SalarioLiquido().ToString("F2"));
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double aumentar = double.Parse(Console.ReadLine());
+            f.AumentarSalario(aumentar);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + f);
+        }
+    }
+}
