c03f27bc23a0c3fcbc134c6451a73ae66d35c381
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-13 18:04:27 -0300

    update: utilizando encapsulamento + properties

--- Sobrecarga/Sobrecarga/Produto.cs
+++ Sobrecarga/Sobrecarga/Produto.cs
@@ -33,27 +33,24 @@ namespace Construtores
         }
 
         //Encapsulamentos
-        public string GetNome()
+        public string Nome
         {
-            return _nome;
-        }
-
-        public void SetNome(string nome)
-        {
-            if (nome == null && nome.Length > 1)
-            {
-                _nome = nome;
+            get { return _nome;  }
+            set { 
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
             }
         }
 
-        public double GetPreco()
+        public double Preco
         {
-            return _preco;
+            get { return _preco; }
         }
-
-        public int GetQuantidade()
+        public int Quantidade
         {
-            return _quantidade;
+            get { return _quantidade; }
         }
 
         //Métodos
--- Sobrecarga/Sobrecarga/Program.cs
+++ Sobrecarga/Sobrecarga/Program.cs
@@ -9,11 +9,10 @@ namespace SobrecargaConstrutores
         {
             Produto p = new Produto("TV", 500.00, 10);
 
-            Console.WriteLine(p.GetNome());
-            p.SetNome("TV 4K");
-            Console.WriteLine(p.GetNome());
-            Console.WriteLine(p.GetPreco());
-            Console.WriteLine(p.GetQuantidade());
+            Console.WriteLine(p.Nome);
+            p.Nome = "TV 4K";
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
         }
     }
 }
