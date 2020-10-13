2651d0c296c231159f20c727af4c1ed12b6419a3
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-13 17:50:46 -0300

    update: utilizando o encapsulamento com métodos get/set

--- Sobrecarga/Sobrecarga/Produto.cs
+++ Sobrecarga/Sobrecarga/Produto.cs
@@ -8,53 +8,77 @@ namespace Construtores
     class Produto
     {
         //Atributos
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
         public Produto()
         {
-            Quantidade = 10;
+            
         }
 
         //Contrutor de 2 argumentos
         public Produto(string nome, double preco) : this() //reaproveitando a quantidade acima
         {
-            Nome = nome;
-            Preco = preco;
+            _nome = nome;
+            _preco = preco;
         }
 
         //Construtor de 3 argumento
         public Produto(string nome, double preco, int quantidade) : this(nome, preco)
         {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = quantidade;
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        //Encapsulamentos
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            if (nome == null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
         }
 
         //Métodos
         public double ValorTotalEmEstoque()
         {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProdutos(int quantidade)
         {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProdutos(int quantidade)
         {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
 
         public override string ToString()
         {
-            return Nome
+            return _nome
                 + ", $ " +
-                Preco.ToString("F2", CultureInfo.InvariantCulture)
+                _preco.ToString("F2", CultureInfo.InvariantCulture)
                 + ", "
-                + Quantidade
+                + _quantidade
                 + " unidades, Total: $ "
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
--- Sobrecarga/Sobrecarga/Program.cs
+++ Sobrecarga/Sobrecarga/Program.cs
@@ -7,35 +7,13 @@ namespace SobrecargaConstrutores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Entre com os dados do produto:");
-            Console.Write("Nome: ");
-            string nome = Console.ReadLine();
-            Console.Write("Preço: ");
-            double preco = double.Parse(Console.ReadLine());
-            //int quantidade = int.Parse(Console.ReadLine());
+            Produto p = new Produto("TV", 500.00, 10);
 
-            //Produto p = new Produto(nome, preco, quantidade); Com 3 argumentos
-            Produto p = new Produto(nome, preco);
-            //Produto p = new Produto(); Sem argumentos
-
-            Produto p2 = new Produto { Nome = "TV", Preco = 500.00, Quantidade = 20 };
-
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto: " + p);
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser adicionados: ");
-            int qte = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qte);
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser removido do estoque: ");
-            qte = int.Parse(Console.ReadLine());
-            p.RemoverProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine(p.GetNome());
+            p.SetNome("TV 4K");
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
         }
     }
 }
