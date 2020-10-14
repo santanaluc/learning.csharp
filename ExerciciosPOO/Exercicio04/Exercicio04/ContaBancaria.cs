c4ac7c73597c658743da582d4e71db856a7a7345
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-14 14:01:59 -0300

    Iniciando...

---
+++ ExerciciosPOO/Exercicio04/Exercicio04/ContaBancaria.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio04
+{
+    class ContaBancaria
+    { 
+        private string _nome;
+        private double _valor;
+        private double _saldo;
+        public int Conta { get; set; }
+
+        public ContaBancaria()
+        {
+
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                } else
+                {
+                    Console.WriteLine("ERROR");
+                }
+            }
+        }
+
+        public double Deposito(double valor)
+        {
+            return _saldo = _valor + valor;
+        }
+
+        public double Saque(double valor)
+        {
+            return _saldo = _valor - valor;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " 
+                + Conta + ", "
+                + "Titlar: " + _nome + ", Saldo: $" + _saldo;
+        }
+    }
+}
---
+++ ExerciciosPOO/Exercicio04/Exercicio04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria c = new ContaBancaria();
+
+            Console.Write("Entre com o número da conta: ");
+            c.Conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            c.Nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char sn = char.Parse(Console.ReadLine());
+
+            if (sn == 's')
+            {
+                Console.Write("Entre com o valor de depósito inicial: ");
+                double deposito = double.Parse(Console.ReadLine());
+                c.Deposito(deposito);
+            } else
+            {
+                return;
+            }
+
+            Console.WriteLine(c);
+        }
+    }
+}
