95e57351d0c0623eabb1535ca9a47ddae8a51ed0
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-14 19:27:07 -0300

    Aprendendo sobre Tipo Referência e Tipo Valor

---
+++ TipoReferencia/TipoReferencia/Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipoReferencia
+{
+    struct Point
+    {
+        public double X, Y;
+        public override string ToString()
+        {
+            return "(" + X + "," + Y + ")";
+        }
+    }
+}
---
+++ TipoReferencia/TipoReferencia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TipoReferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+            //Console.WriteLine(p); // erro: variável não atribuída
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p);
+
+            p = new Point();
+            Console.WriteLine(p);
+        }
+    }
+}
