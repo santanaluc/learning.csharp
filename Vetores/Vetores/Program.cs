a5cb6a18985ba05466718843597605e77032e227
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-15 17:43:16 -0300

    Aprendendo sobre vetores -- parte 1

---
+++ Vetores/Vetores/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine());
+            }
+
+            double soma = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                soma += vect[i];
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine("Altura média = " + media.ToString("F2"));
+        }
+    }
+}
---
+++ Vetores/Vetores/ProgramPt2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vetores
+{
+    class ProgramPt2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
