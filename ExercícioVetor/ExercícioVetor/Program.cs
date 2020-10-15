2da663665d2e551e61d98895e11ea28f497fc663
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-15 19:19:36 -0300

    Exercício proposto para a pratica de vetores.

---
+++ ExercícioVetor/ExercícioVetor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ExercícioVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quartos[] vect = new Quartos[10];
+            
+            Console.Write("Quantos quartos serão reservados? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < n; i++)
+            {
+                int numRoom = i + 1;
+                Console.WriteLine("Aluguel #" + numRoom);
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int room = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                vect[room] = new Quartos(name, email);
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
---
+++ ExercícioVetor/ExercícioVetor/Quartos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercícioVetor
+{
+    class Quartos
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+        public Quartos(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+    }
+}
