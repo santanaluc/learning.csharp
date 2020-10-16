11a41d497dff1ca8ab6f1525e2cf9fb993f4e0ee
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-16 01:07:50 -0300

    Aprendendo sobre a utilização do modificador de paramêtros "params"

---
+++ ModificadorParametros/ModificadorParametros/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModificadorParametros
+{
+    class Calculator
+    {
+        //Sem o params, precisaria instânciar o vetor no programa principal
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ ModificadorParametros/ModificadorParametros/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ModificadorParametros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = Calculator.Sum(2, 3);
+            int soma2 = Calculator.Sum(2, 4, 3);
+
+            Console.WriteLine(soma);
+            Console.WriteLine(soma2);
+        }
+    }
+}
