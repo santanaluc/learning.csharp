f27b1b7af54a7add1926ed00a9ffbfb6062d1cb6
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-17 17:22:32 -0300

    Aprendendo a estrutura ForEach

---
+++ ForEach/Foreach/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = new string[] { "Maria", "Bob", "Alex" };
+
+            Console.WriteLine("FOREACH:");
+            //Para cada objeto 'obj' contido em vet, faça:
+            foreach (string obj in vet)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("FOR NORMAL:");
+            //Com for normal:
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+        }
+    }
+}
