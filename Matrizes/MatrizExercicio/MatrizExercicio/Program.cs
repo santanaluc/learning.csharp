4d59d382f04a0eb362ecd535cb7f3afc60452372
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-21 00:33:59 -0300

    explicação e exercício de matrizes

---
+++ Matrizes/MatrizExercicio/MatrizExercicio/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MatrizExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tamanho = Console.ReadLine().Split(' ');
+
+            int M = int.Parse(tamanho[0]);
+            int N = int.Parse(tamanho[0]);
+
+            int[,] mat = new int[M, N];
+
+            //Linhas
+            for (int i = 0; i < M; i++)
+            {
+                //criar um vetor com split
+                string[] values = Console.ReadLine().Split(' ');
+
+                //Colunas
+                for (int j = 0; j < N; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            int num = int.Parse(Console.ReadLine());
+            //Posição
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if(mat[i,j] == num)
+                    {
+                        Console.WriteLine("Position: " + i + ", " + j);
+
+                        if (M > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                    }
+                    if (i > 0)
+                    {
+                        Console.WriteLine("Up: " + mat[i - 1, j]);
+                    }
+                    if (j < N - 1)
+                    {
+                        Console.WriteLine("Right: " + mat[i, j + 1]);
+                    }
+                    if (i < M - 1)
+                    {
+                        Console.WriteLine("Down: " + mat[i + 1, j]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Matrizes/MatrizExplicacao/MatrizExplicacao/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MatrizExplicacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //indica que criará um arranjo bi dimensional
+            /*double[,] mat = new double[2, 3];
+            
+            Console.WriteLine(mat.Length);
+
+            //quanto é a primeira dimensão da matriz -- quantidade de linhas
+            Console.WriteLine(mat.Rank);
+
+            //Dimensão 0 da matriz tem tamanho 2
+            Console.WriteLine(mat.GetLength(0));
+            Console.WriteLine(mat.GetLength(1));*/
+
+            Console.Write("Digite o tamanho da matriz: ");
+            int tamanho = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[tamanho, tamanho];
+            Console.WriteLine();
+            //Linhas
+            for (int i = 0; i < tamanho; i++)
+            {
+                //criar um vetor com split
+                string[] values = Console.ReadLine().Split(' ');
+
+                //Colunas
+                for (int j = 0; j < tamanho; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Main diagonal: ");
+            for (int i = 0; i < tamanho; i++)
+            {
+                Console.Write(mat[i,i] + " ");
+            }
+            Console.WriteLine();
+
+            int count = 0;
+            for(int i = 0; i < tamanho; i++)
+            {
+                for (int j = 0; j < tamanho; j++)
+                {
+                    if (mat[i,j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.Write("Negative numbers: " + count);
+            Console.WriteLine();
+        }
+    }
+}
