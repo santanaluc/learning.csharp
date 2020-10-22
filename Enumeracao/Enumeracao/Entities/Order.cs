832bbaf0e3e48e423f75c00c94f0ac6739ffd501
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-22 11:17:55 -0300

    Aprendendo sobre design, organização, entidades e Enums

---
+++ Enumeracao/Enumeracao/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Enumeracao.Entities.Enums;
+
+namespace Enumeracao.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", " + Moment + ", " + Status;
+        }
+    }
+}
---
+++ Enumeracao/Enumeracao/Program.cs
@@ -0,0 +1,28 @@
+﻿using Enumeracao.Entities;
+using Enumeracao.Entities.Enums;
+using System;
+
+namespace Enumeracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            // conversão de tipos
+            string txt = OrderStatus.PendingPayment.ToString(); // tipo enum para string 
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); //tem que ser igual ao que está descrito no Enum
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        }
+    }
+}
