87c9cbef8177c3ce7463c6769977d5130b98f19b
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-23 01:19:12 -0300

    Exercicio baseado em POO + utilizacao do StringBuilder + Entidades

---
+++ ExerciciosPOO/Exercicio05/Exercicio05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Exercicio05.Entities;
+
+namespace Exercicio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("Wow that's awesome!");
+            Post p1 = new Post(
+                DateTime.Parse("10/22/2020 00:59:59"), 
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country!", 
+                12);
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good Night");
+            Comment c4 = new Comment("May the Force be with you");
+            Post p2 = new Post(
+                DateTime.Parse("10/22/2020 01:12:00"),
+                "Good night guys",
+                "See you tomorrow",
+                2);
+
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
