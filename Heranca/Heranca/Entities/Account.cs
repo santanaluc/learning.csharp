0e8a337de8a4a41c4f78ae7eee42406250137a28
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-25 01:39:26 -0300

    pratica dos conceitos de heranca de classes.

---
+++ Heranca/Heranca/Entities/Account.cs
@@ -0,0 +1,32 @@
+﻿namespace Heranca.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        //modifica somente na sub-classe.
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ Heranca/Heranca/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Heranca.Entities;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Permite o reuso de atributos e métodos 
+            //Relacao "é-um"
+            // Heranca é uma associação entre classes (e nao entre objetos)
+
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+            Console.WriteLine(account.Balance);
+
+        }
+    }
+}
