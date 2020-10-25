9929ab9bdaa08ef4d98978a559c29a209e5bddd1
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-25 02:20:04 -0300

    adicionando conceitos de sealed

--- Heranca/Heranca/Entities/SavingsAccount.cs
+++ Heranca/Heranca/Entities/SavingsAccount.cs
@@ -1,6 +1,6 @@
 ﻿namespace Heranca.Entities
 {
-    class SavingsAccount : Account
+    sealed class SavingsAccount : Account
     {
         public double InterestRate { get; set; }
 
---
+++ Heranca/Heranca/Entities/SavingsAccountPlus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca.Entities
+{
+    class SavingsAccountPlus : SavingsAccount
+    {
+
+        //Não é possível herdar elementos da classe "SavingsAccount pois ela está com a palavra "sealed"."
+        //Serve para proteger alguma regra de negócio que não pode ser exposta
+    }
+}
