99fd3c23eaebf5e0c079e7788a2fcdf502970bbc
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-25 12:56:54 -0300

    Exercício baseado em heranca e poliformismo - adicionando entidades

---
+++ Exercicios/Exercicio7/Exercicio7/Entities/OutSourcedEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio7.Entities
+{
+    class OutSourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutSourcedEmployee()
+        {
+
+        }
+
+        public OutSourcedEmployee(double additionalCharge)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            base.Payment();
+            return AdditionalCharge *= 0.1;
+        }
+    }
+}
