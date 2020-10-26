39e079635851f4f0d5fc44576139e098c345753e
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-26 17:03:55 -0300

    Exercicio try-catch

---
+++ Exercicios/Exercicio12/Exercicio12/Entities/Account.cs
@@ -0,0 +1,50 @@
+﻿using Exercicio12.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio12.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        public Account()
+        {
+
+        }
+        public Account(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+             Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if (WithDrawLimit < amount)
+            {
+                throw new DomainException("Withdraw error: The amount exceedss withdraw limit.");
+            } 
+            if (Balance < amount)
+            {
+                throw new DomainException("Withdraw error: Not enough balance.");
+            }
+            Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return "New balance: " + Balance.ToString("F2");
+        }
+    }
+}
---
+++ Exercicios/Exercicio12/Exercicio12/Program.cs
@@ -0,0 +1,44 @@
+﻿using Exercicio12.Entities;
+using Exercicio12.Entities.Exceptions;
+using System;
+
+namespace Exercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data:!");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string name = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double initial = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                double limit = double.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+                Account acc = new Account(number, name, initial, limit);
+                Console.Write("Enter amount for withdraw: ");
+                double withdraw = double.Parse(Console.ReadLine());
+                acc.WithDraw(withdraw);
+                Console.WriteLine(acc);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
