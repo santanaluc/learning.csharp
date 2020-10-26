db4661f82ed6abb44851df9993006ffe8592d119
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-25 23:56:21 -0300

    Exercicio baseado em heranca e poliformismo

---
+++ Exercicios/Exercicio8/Exercicio8/Entities/ImportedProduct.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio8.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+
+        }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return base.PriceTag() + CustomsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price += CustomsFee;
+        }
+    }
+}
---
+++ Exercicios/Exercicio8/Exercicio8/Entities/UsedProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio8.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime Date { get; set; }
+
+        public UsedProduct()
+        {
+
+        }
+
+        public UsedProduct(string name, double price, DateTime date) : base(name, price)
+        {
+            Date = date;
+        }
+
+        public override string PriceTag()
+        {
+            return base.PriceTag();
+        }
+    }
+}
