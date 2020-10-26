733e1b69fb83892a95f02c4fa1c89f7ec5ba874a
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-26 18:08:11 -0300

    Lendo o arquivo inteiro

--- Files/FileStream-StreamReader/FileStream-StreamReader/Program.cs
+++ Files/FileStream-StreamReader/FileStream-StreamReader/Program.cs
@@ -13,8 +13,11 @@ namespace FileStream_StreamReader
             try
             {
                 sr = File.OpenText(path);
-                string line = sr.ReadLine();
-                Console.WriteLine(line);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
             }
             catch (IOException e)
             {
