80f2a36b353d263e6c5a19056d2d1d41fa6e2226
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-26 18:04:30 -0300

    Forma não resumida de utilizar o FileStream

---
+++ Files/FileStream-StreamReader/FileStream-StreamReader/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace FileStream_StreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\csharp\nivelamento\ws-2\File1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            //Parte nao resumida
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error acurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                //Fechar as strings
+                if (sr != null) sr.Close();
+                if (fs != null) sr.Close();
+            }
+        }
+    }
+}
