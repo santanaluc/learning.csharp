29984fa48e315348ca278c54428849a9ebfbfa57
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-26 18:16:22 -0300

    utilizando using para diminuir o código e ganhar performance

---
+++ Files/usingblock/UsingBlock/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace UsingBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\csharp\nivelamento\ws-2\File1.txt";
+
+            //Tratando erro
+            try
+            {
+                //executado e fechado
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    //será executado e fechado
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
