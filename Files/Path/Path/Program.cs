198b75dfde709bef7e29d9695a2a1b89c60f3a30
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-26 19:59:19 -0300

    Utilizando a classe path

---
+++ Files/Path/Path/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace PathX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Path realiza operacoes com strings que contém informações de arquivos ou pastas");
+            Console.WriteLine();
+
+            string path = @"D:\csharp\nivelamento\ws-2\myfolder\File1.txt";
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path)); // pega somente as pastas
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar); // somente a barra invertida
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator); // ; é utilizado para separar a pasta
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+        }
+    }
+}
