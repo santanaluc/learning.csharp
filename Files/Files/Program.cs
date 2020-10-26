0fd5a1b5a0720ac2be5a6f31fc1cb1ea6a1acc5b
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-26 17:36:34 -0300

    File, FileInfo, IOException

---
+++ Files/Files/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"D:\csharp\nivelamento\ws-2\File1.txt";
+            string targetPath = @"D:\csharp\nivelamento\ws-2\File2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
