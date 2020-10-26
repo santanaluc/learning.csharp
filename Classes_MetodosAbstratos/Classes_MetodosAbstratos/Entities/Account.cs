e9dedfd116cf2ab4665d2179ea3dffd1014703c6
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-26 00:42:01 -0300

    Prática dos conceitos de classes abstratas

---
+++ Classes_MetodosAbstratos/Classes_MetodosAbstratos/Entities/Account.cs
@@ -0,0 +1,33 @@
+﻿namespace Classes_MetodosAbstratos.Entities
+{
+    abstract class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        //modifica somente na sub-classe.
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount + 5.0;
+            //Pode ser sobrescrito nas sub-classe (virtual)
+        }
+
+        public virtual void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
