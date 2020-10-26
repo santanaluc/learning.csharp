6e1492a75f1ebbef0600cdd509f9a58f5b6a05b2
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-26 19:26:20 -0300

    Utilização do StreamWriter para escrever em um novo arquivo

---
+++ Files/StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWriterX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"D:\csharp\nivelamento\ws-2\File1.txt";
+            string targetPath = @"D:\csharp\nivelamento\ws-2\File2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach(string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                } 
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
