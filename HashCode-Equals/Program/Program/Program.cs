bf9e869040538587b6b14f8f03b563d7e74aa8f8
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-31 02:22:38 -0300

    utilizando GetHashCode() e Equals()

---
+++ HashCode-Equals/Program/Program/Program.cs
@@ -0,0 +1,19 @@
+﻿using Program.Entities;
+using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b); // == compara a referencia do ponteiro de memória do objeto
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
