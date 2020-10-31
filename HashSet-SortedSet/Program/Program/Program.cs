af1a6aff22e580c8365371cdaed2d47e17c74516
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-10-31 02:37:17 -0300

    Exemplo utilizando HashSet

---
+++ HashSet-SortedSet/Program/Program/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Computer"));
+            Console.WriteLine();
+            foreach (string i in set)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
