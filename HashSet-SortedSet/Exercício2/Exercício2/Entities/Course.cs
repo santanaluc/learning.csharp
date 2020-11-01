5f08b3b7b691adedab0a5212c0e799005df9feda
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-11-01 14:36:14 -0300

    Version 1.0 - Exercício baseado em sets

---
+++ HashSet-SortedSet/Exercício2/Exercício2/Entities/Course.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício2.Entities
+{
+    class Course
+    {
+        public int Code { get; set; }
+
+        public Course(int code)
+        {
+            Code = code;
+        }
+
+        public override int GetHashCode()
+        {
+            return Code.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Course))
+            {
+                return false;
+            }
+
+            Course other = obj as Course;
+            return Code.Equals(other.Code);
+        }
+
+        public override string ToString()
+        {
+            return "Code" + Code;
+        }
+    }
+}
---
+++ HashSet-SortedSet/Exercício2/Exercício2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Exercício2.Entities;
+
+namespace Exercício2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Course> set = new HashSet<Course>();
+
+            Console.Write("How many students for course A? ");
+            int nA = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nA; i++)
+            {
+                int student = int.Parse(Console.ReadLine());
+                set.Add(new Course(student));
+            }
+
+            Console.Write("How many students for course B? ");
+            int nB = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nB; i++)
+            {
+                int student = int.Parse(Console.ReadLine()); 
+                set.Add(new Course(student));
+
+            }
+
+            Console.Write("How many students for course C? ");
+            int nC = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nC; i++)
+            {
+                int student = int.Parse(Console.ReadLine());
+                set.Add(new Course(student));
+            }
+
+            Console.WriteLine($"Total of {set.Count} students.");
+            
+            
+            /*TESTE
+            foreach (Course obj in set)
+            {
+                Console.WriteLine(obj);
+            }*/
+
+            //PrintCollection(set);
+        }
+
+        //Teste
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
