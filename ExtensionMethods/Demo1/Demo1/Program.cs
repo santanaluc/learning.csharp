168efa0335a37449576fb1d028ea80b47a4998ce
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-11-02 16:37:37 -0300

    Aprendendo sobre extension methods

---
+++ ExtensionMethods/Demo1/Demo1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2020, 11, 2, 8, 10, 45);
+
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
---
+++ ExtensionMethods/Demo2/Demo2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = "Good morning dear students!";
+
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
