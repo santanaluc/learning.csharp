bfcc71e2fafc44cee612e5edc446b63281710072
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-11-02 21:04:34 -0300

    Introdução de multicast com delegates

---
+++ secao17/MulticastDemo/MulticastDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using MulticastDemo.Services;
+using System;
+
+namespace MulticastDemo
+{
+    class Program
+    {
+        delegate void BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            var a = 10.0;
+            var b = 12.0;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            //DIRETAMENTE POIS É DO TIPO VOID
+            op(a, b);
+            //op.Invoke(a,b); mesma coisa
+
+        }
+    }
+}
