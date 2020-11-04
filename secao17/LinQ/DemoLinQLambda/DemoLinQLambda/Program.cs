bfa75916c62c012db4dde6b12a5acbff0fa5a8de
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-11-04 15:45:39 -0300

    utilizando a sintaxe de consulta SQL

--- secao17/LinQ/DemoLinQLambda/DemoLinQLambda/Program.cs
+++ secao17/LinQ/DemoLinQLambda/DemoLinQLambda/Program.cs
@@ -39,25 +39,62 @@ namespace DemoLinQLambda
                 new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
             };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            //MESMA COISA MAS COM NOTAÇÃO DE SQL
+            var r1 =
+                from p in products
+                where p.Category.Tier == 1 && p.Price < 900.0
+                select p;
+
             Print("TIER 1 AND PRICE < 900:", r1);
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 =
+                from p in products
+                where p.Category.Name == "Tools"
+                select p.Name;
+
             Print("NAMES OF PRODUCTS FROM TOOLS:", r2);
 
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryNmae = p.Category.Name });
+            // var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryNmae = p.Category.Name });
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new
+                {
+                    p.Name,
+                    p.Price,
+                    CategoryName = p.Category.Name
+                };
+
             Print("NAMES STARTED WITH 'C' AND ANOUNYMOUS OBJECT:", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Name
+                orderby p.Price
+                select p;
+
             Print("TIER 1 ORDER BY PRICE THE BY NAME:", r4);
 
-            var r5 = r4.Skip(2).Take(4);
+            //OPERACOES ESPECIAIS
+            //var r5 = r4.Skip(2).Take(4);
+            var r5 =
+                (from p in r4
+                 select p).Skip(2).Take(4);
+
             Print("TIER 1 ORDER BY PRICE THE BY NAME SKIP 2 TAKE 4:", r4);
 
-            var r6 = products.FirstOrDefault();
+            //var r6 = products.FirstOrDefault();
+            var r6 =
+                (from p in products select p).FirstOrDefault();
+
             Console.WriteLine("First or default test 1 = " + r6);
 
-            var r7 = products.Where(p => p.Price > 3000.00).FirstOrDefault();
+            // var r7 = products.Where(p => p.Price > 3000.00).FirstOrDefault();
+            var r7 = (from p in products where p.Price > 3000.00 select p).FirstOrDefault();
             Console.WriteLine("First or default test 2 = ", r7);
             Console.WriteLine();
 
@@ -87,7 +124,10 @@ namespace DemoLinQLambda
             Console.WriteLine("Category 1 aggregate sum: " + r15);
             Console.WriteLine();
 
-            var r16 = products.GroupBy(p => p.Category);
+            //var r16 = products.GroupBy(p => p.Category);
+            var r16 =
+                from p in products
+                group p by p.Category;
             foreach(var group in r16)
             {
                 Console.WriteLine("Category " + group.Key.Name + ":");
