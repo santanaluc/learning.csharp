0da655cebbeda4b2d021cf8d40d5ab3f0ed47914
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-11-04 00:35:21 -0300

    Demonstração de Linq + Lambda

---
+++ secao17/LinQ/DemoLinQLambda/DemoLinQLambda/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoLinQLambda.Entities
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", " + Price.ToString("F2") + ", " + Category.Name + ", " + Category.Tier;
+        }
+    }
+}
