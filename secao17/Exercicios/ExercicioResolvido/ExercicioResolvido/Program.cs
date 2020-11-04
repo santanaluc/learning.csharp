9d2daf82790f9b9783b46ca1672d0404aad05d84
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-11-04 16:33:39 -0300

    Exercicio utilizando Linq e arquivos

--- secao17/Exercicios/ExercicioResolvido/ExercicioResolvido/Program.cs
+++ secao17/Exercicios/ExercicioResolvido/ExercicioResolvido/Program.cs
@@ -10,8 +10,9 @@ namespace ExercicioResolvido
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter full file path: ");
-            string path = Console.ReadLine();
+            /*Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();*/
+            string path = @"D:\csharp\Temp\in3.txt";
 
             List<Product> list = new List<Product>();
 
