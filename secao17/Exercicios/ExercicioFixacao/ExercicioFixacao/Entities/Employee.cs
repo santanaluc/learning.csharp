165941f3e99ebdb0dbee6c7c56f227ceee27efd3
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-11-04 16:56:36 -0300

    Exercício proposto para a prática de LinQ

---
+++ secao17/Exercicios/ExercicioFixacao/ExercicioFixacao/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace ExercicioFixacao.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
---
+++ secao17/Exercicios/ExercicioFixacao/ExercicioFixacao/Program.cs
@@ -0,0 +1,48 @@
+﻿using ExercicioFixacao.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ExercicioFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();*/
+
+            //LENDO O ARQUIVO E COLOCANDO DENTRO DA LISTA / ENTIDADE
+            var path = @"D:\csharp\Temp\in4.txt";
+            Console.Write("Enter salary: ");
+            var enterSalary = double.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    var name = fields[0];
+                    var email = fields[1];
+                    var salary = double.Parse(fields[2]);
+
+                    list.Add(new Employee(name, email, salary));
+                }
+            }
+            // TERMINOU DE LER O ARQUIVO E COLOCAR NA LISTA
+
+            Console.WriteLine("Email of people whose salary is more than " + enterSalary.ToString("F2"));
+            var emailSalary = list.Where(e => e.Salary > enterSalary).Select(e => e.Email);
+            foreach (var email in emailSalary)
+            {
+                Console.WriteLine(email);
+            }
+
+            var sumSalary = list.Where(e => e.Name[0] == 'M').Sum(e => e.Salary);
+            Console.WriteLine("Sum of salary of people whose name stats with 'M': " + sumSalary.ToString("F2"));
+        }
+    }
+}
