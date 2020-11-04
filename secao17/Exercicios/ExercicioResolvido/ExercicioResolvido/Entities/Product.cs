4092c3290f87e263b8016a9b7ed3eb16a5cb94ec
Lucas Santana <lucas.s.s.silva@hotmail.com>
2020-11-04 16:30:13 -0300

    Exercicio utilizando Linq e arquivos

---
+++ secao17/Exercicios/ExercicioResolvido/ExercicioResolvido/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace ExercicioResolvido.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ secao17/Exercicios/ExercicioResolvido/ExercicioResolvido/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ExercicioResolvido.Entities;
+
+namespace ExercicioResolvido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    var name = fields[0];
+                    var price = double.Parse(fields[1]);
+
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price = " + avg.ToString("F2"));
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
